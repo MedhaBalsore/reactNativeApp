b4665b4d4597cfb0b4aa95874f4bbc054129f52a
BevinKistnasamy <bevin.kistnasamy@umail.uom.ac.mu>
2023-06-08 11:08:59 +0400

    2nd Test

--- screens/Health.js
+++ screens/Health.js
@@ -1,12 +1,100 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, Image } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+
+import { useEffect, useRef, useState } from 'react';
+import {Camera} from 'expo-camera'
+import { shareAsynch } from 'expo-sharing';
+import * as MediaLibrary from 'expo-media-library';
 
 const Health = () => {
+
+
+
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasMediaLibraryPermission, sethasMediaLibraryPermission] = useState();
+  const [photo, setPhoto] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back)
+  const [flash,  setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef();
+  useEffect(() => {
+    (async () => {
+      
+      MediaLibrary.requestPermissionsAsync();
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraPermission.status === 'granted');
+      setHasMediaLibraryPermission(mediaLibraryPermission.status === 'granted');
+    })();
+  }, []);
+
+  if (hasCameraPermission === undefined) {
+    return <Text>Requesting permissions...</Text>
+  } else if (!hasCameraPermission) {
+    return <Text>Permission for camera not granted. Please change this in settings.</Text>
+  }
+
+  let takePic = async () => {
+    let options = {
+      quality: 1,
+      base64: true,
+      exif: false
+    };
+
+    let newPhoto = await cameraRef.current.takePictureAsync(options);
+    setPhoto(newPhoto);
+  };
+
+  if (photo) {
+    let sharePic = () => {
+      shareAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+      });
+    };
+
+    let savePhoto = () => {
+      MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+      });
+    };
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
+        <Button title="Share" onPress={sharePic} />
+        {hasMediaLibraryPermission ? <Button title="Save" onPress={savePhoto} /> : undefined}
+        <Button title="Discard" onPress={() => setPhoto(undefined)} />
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <View>
-      <Text>the health screen tab</Text>
-    </View>
+    <Camera style={styles.container} ref={cameraRef} flashmode={flash} type={type}>
+      <View style={styles.buttonContainer}>
+        <Button title="Take Pic" onPress={takePic} />
+      </View>
+      <StatusBar style="auto" />
+    </Camera>
   );
 };
 
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    backgroundColor: '#fff',
+    alignSelf: 'flex-end'
+  },
+  preview: {
+    alignSelf: 'stretch',
+    flex: 1
+  }
+});
 export default Health;
+
